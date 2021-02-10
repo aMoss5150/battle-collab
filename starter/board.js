b99c1d58d09bb5083263fca935dd91d602a411cf
Andrew Moss <andrewmoss@Andrews-MacBook-Pro.local>
2021-02-10 15:54:45 -0600

    array made -am

--- starter/board.js
+++ starter/board.js
@@ -9,19 +9,36 @@ class Board {
     //       2 [0,2] [1,2] [2,2] [3,2] [4,2]
     //       3 [0,3] [1,3] [2,3] [3,3] [4,3]
     //       4 [0,4] [1,4] [2,4] [3,4] [4,4]
-
+    this.numRows = 10;
+    this.numCols = 10;
+    this.numShips = 20;
+    this.grid = this.populateGrid()
 
   }
-  
+
   populateGrid() {
     // TODO: Using the instance variables numRows, numCols, and numShips, return
     // a 2D array representing the state of the board.
+    let array = [];
+    for (let i = 0; i < this.numRows; i++) {
+      let array2 = [];
+
+      for (let j = 0; j < this.numCols; j++) {
+        array2.push(null)
+      }
+      array.push(array2)
+
+    }
+    // console.log('array:', array)
+    return array;
+
   }
 
   display() {
     // TODO: Print the game board with marks on any spaces that have been fired
     // upon. Be sure not to display the unhit ships to the user! Hint: you might
     // be able to use console.table()
+    console.table(this.populateGrid())
   }
 
   count() {
@@ -44,6 +61,8 @@ class Board {
   }
 }
 
-console.table([[0, 45, 2, 3], [0, 1, 2 ,3], [0, 1 , 2, 3], [0, 1 , 2, 3]]);
+// console.table([[0, 45, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]]);
+const board = new Board;
+board.display()
 
 module.exports = Board;
